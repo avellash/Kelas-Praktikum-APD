36f37f13366a3321eda66cef5c3aa4a6e12566a3
avellash <avellash.tik@gmail.com>
2024-09-25 09:25:21 +0800

    Add files via upload

---
+++ Pertemuan-4/2409106045_AvellaSalsabellaH_A2_24.py
@@ -0,0 +1,114 @@
+# for j in range(12):
+#     print("halo")
+# print("hai")
+
+# for j in range(2,12,2:)
+#     print("halo")
+# print("hai")
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("--------------------------------")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i in simpan:
+#     print(i)
+# for j in range(12):
+#     print("halo")
+# print("hai")
+
+# for j in range(2,12,2:)
+#     print("halo")
+# print("hai")
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("--------------------------------")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i in simpan:
+#     print(i)
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("--------------------------------")
+# simpan = ["Nasi Goreg", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i, menu in enumerate(simpan,start=1):
+#     print(f"{i}.{menu}")
+
+# makanan = ["mie", "sop", "bakso"]
+# minuman = ["es teh", "air putih", "es jeruk"]
+
+# for i in makanan :
+#     for j in minuman:
+#         print(f"{i} & {j}")
+
+# while True:
+#     print("Hello World")
+
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya' or jawab == 'Ya'):
+#     hitung += 1
+#     jawab = input("Ulang lagi tidak ")
+#     print(f"Total perulangan: {hitung}")
+
+# i = 0
+# while i < 5:
+#     print(i)
+#     i += 1
+
+# i = 0
+# while i < 5:
+#     print(i)
+#     break
+#     i += 1
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin Mengulang ")
+#     if ulang == "tidak" or ulang =="Tidak":
+#         print("Oke kita udahan")
+#         break
+
+# print(f"Total Perulangan: {hitung}")
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin Lanjut ")
+#     if ulang == "y" or ulang =="Y":
+#         print("Oke Kita Lanjut")
+#         continue
+
+# print(f"Total Perulangan: {hitung}")
+
+# print("Dftar bilangan ganjil dari 1-10")
+# for i in range(10):
+#     if i % 2 == 0:
+#         print(f"{i} genap")
+#         # continue
+#     else:
+#         print(f"{i} ganjil")
+#         # continue
+
+# total = 0
+# while True:
+#     angka int(input("Masukkan angka positif (input negatif untuk berhenti): "))
+#     if angka < 0:
+#         break
+#     total += angka
+
+# print("jumlah total adalah:, total")
+
+# hitung_prima = 0
+
+# for angka in range(1, range_maks):
+#     angka += 1
+#     apakah_prima = True
+
+#     for i in range(2, angka):
+#         if angka % i == 0:
+#             apakah_prima = False
+#             break
+#     if apakah_prima == True:
+#         hitung_prima += 1
+#         print(f"{angka} = prima")
+
+# print(f"bilangan prima dari range 1 - {range_maks} ada = {hitung_prima}")
